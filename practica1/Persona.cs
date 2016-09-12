5e741f8a97fd5624db7350591700131d0cab1d45
Moyses Nev <moyrene84@gmail.com>
2016-09-11 19:53:30 -0700

    segundo

---
+++ practica1/Persona.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practica1
+{
+    public class Persona
+    {
+        public int Edad { get; set; }
+        public string Nombre { get; set; }
+
+
+        public void despliegaNombre()
+        {
+            Console.WriteLine("El nombre es: {0}", Nombre);
+        }
+
+        public void despliegaEdad()
+        {
+            Console.WriteLine("La edad del Alumno es: {0} ", Edad);
+            
+        }
+
+       
+    }
+}
