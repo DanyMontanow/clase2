65c261f4f019e11683399408c163343385d3dc08
Moyses Nev <moyrene84@gmail.com>
2016-09-11 20:42:11 -0700

    segundo

---
+++ practica1/Profesor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practica1
+{
+    public class Profesor : Persona
+    {
+        public float Sueldo { get; set; }
+        public string Materia { get; set; }
+
+
+    }
+}
