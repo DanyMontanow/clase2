d3877c7dc03e4112f4df13a1bef723797cc2b00b
salma <danielamontano2102@gmail.com>
2016-09-13 09:43:34 -0700

    Clases2

---
+++ practica1/practica1/Alumno.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practica1
+{
+    public class Alumno : Persona
+    {
+        public int NumeroControl { get; set; }
+        public string Grupo { get; set; }
+
+       
+
+       
+
+    }
+}
